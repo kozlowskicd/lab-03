369c2f42e9c939ac8d013f0b917d59b74c10f9f6
Chris Kozlowski <kozlowskicd@gmail.com>
2018-10-10 20:18:37 -0700

    add write function, assignment 1 complete

--- edit-file.js
+++ edit-file.js
@@ -1,12 +1,22 @@
 'use strict';
 
 const fs = require('fs');
+const faker = require('faker');
+
 let file = process.argv[2];
 console.log(file);
+
 const returnDataAsText = (err, data) => {
   if (err) {throw err;}
   let dataContent = data.toString().trim();
-  console.log(dataContent);
+  console.log(`Reading file, contents: ${dataContent}`);
 };
+
+fs.readFile(file, returnDataAsText);
+
+fs.appendFile(file, `, ${faker.random.word()}`, (err) => {
+  if (err) {throw err;}
+  console.log(`Writing to file: ${file}`);
+});
 
 fs.readFile(file, returnDataAsText);
